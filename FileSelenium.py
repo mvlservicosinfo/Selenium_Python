7ec3e3252cb5e68dd782131b1f9b0cc11c6b5cde
mvlservicosinfo <60225498+mvlservicosinfo@users.noreply.github.com>
2024-07-01 11:19:57 -0300

    "Commit Selenium Executado"

--- FileSelenium.py
+++ FileSelenium.py
@@ -4,24 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service = Service()
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
+class WebSelenium(object):
+   service = Service()
+   options = webdriver.ChromeOptions()
+   driver = webdriver.Chrome(service=service, options=options)
 
+   url = "https://books.toscrape.com/index.html"
 
-url = "https://books.toscrape.com/index.html"
-driver.get(url)
-time.sleep(5)
+   driver.get(url)
+   time.sleep(5)
 
-links = driver.find_elements(By.CLASS_NAME, 'product_pod')
-lista = []
+   links = driver.find_elements(By.CLASS_NAME, 'product_pod')
+   lista = []
 
-for s in links:
-   lista.append(s.text)
-print(lista)
+   for s in links:
+      lista.append(s.text)
+      print(lista)
 
-data = {"Titulo": lista}
+   data = {"Titulo": lista}
 
-dados = pd.DataFrame(data)
+   dados = pd.DataFrame(data)
 
-dados.to_excel("data.xlsx")
+   dados.to_excel("data.xlsx")
